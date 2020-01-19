7fca1a81f1b8fb47a79e45c790c5a1790d1e81fd
estela19 <e9857256@naver.com>
2020-01-19 17:46:21 +0900

    Feat(Random) : Click buttons and generate random brand

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace IceWall_Random
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "학";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "생";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(298, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "보";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(418, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 69);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "안";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(545, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 69);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "그";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(665, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 69);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "룹";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(161, 299);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 69);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "아";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(294, 299);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 69);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "이";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(429, 299);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 69);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "스";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(568, 299);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 69);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "월";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(825, 488);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace IceWall_Random
+{
+    public partial class Form1 : Form
+    {
+
+        public static ArrayList ContentList = new ArrayList (new string[]{ "과자", "술", "화장품", "라면", "항공사", "전자기기 회사", "치킨", "피자", "은행", "베스킨라빈스", "속옷브랜드", "명품브랜드" });
+
+        public Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void choice()
+        {
+            int idx = rand.Next(0, ContentList.Count);
+            string tmp = ContentList[idx].ToString();
+            ContentList.RemoveAt(idx);
+            MessageBox.Show(tmp, "두-둥!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            choice();
+        }
+
+    }
+}
